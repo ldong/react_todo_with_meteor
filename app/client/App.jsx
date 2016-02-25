783b30f3ae69dfed86171396818b7d711404da24
Lin Dong <ldong@gatech.edu>
2016-02-24 21:03:44 -0800

    Add add new task support

--- app/client/App.jsx
+++ app/client/App.jsx
@@ -14,12 +14,36 @@ App = React.createClass({
       return <Task key={task._id} task={task} />;
     });
   },
+
+
+  handleSubmit(event) {
+    event.preventDefault();
+ 
+    // Find the text field via the React ref
+    let text = ReactDOM.findDOMNode(this.refs.textInput).value.trim();
+ 
+    Tasks.insert({
+      text: text,
+      createdAt: new Date() // current time
+    });
+ 
+    // Clear form
+    React.findDOMNode(this.refs.textInput).value = '';
+  },
+
  
   render() {
     return (
       <div className="container">
         <header>
           <h1>Todo List</h1>
+ 
+          <form className="new-task" onSubmit={this.handleSubmit} >
+            <input
+              type="text"
+              ref="textInput"
+              placeholder="Type to add new tasks" />
+          </form>
         </header>
  
         <ul>
