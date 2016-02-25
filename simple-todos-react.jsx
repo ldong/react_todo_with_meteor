189323ec5b2e768acb036379c834afdb515e1d37
Lin Dong <ldong@gatech.edu>
2016-02-24 22:51:04 -0800

    Add private and public toggle button

--- simple-todos-react.jsx
+++ simple-todos-react.jsx
@@ -8,6 +8,8 @@ if (Meteor.isClient) {
     passwordSignupFields: 'USERNAME_ONLY'
   });
 
+  Meteor.subscribe('tasks');
+
   Meteor.startup(function() {
     ReactDOM.render(<App />, document.getElementById('container'));
   });
@@ -35,11 +37,32 @@ Meteor.methods({
     Tasks.update(taskId, {
       $set: {checked: setChecked}
     });
+  },
+
+  setPrivate(taskId, setToPrivate) {
+    const task = Tasks.findOne(taskId);
+
+    if (task.owner !== Meteor.userId()) {
+      throw new Meteor.Error('not-authorized');
+    }
+
+    Tasks.update(taskId, {$set: {private: setToPrivate} });
   }
+
 
 });
 
 if (Meteor.isServer) {
+  Meteor.publish('tasks', function(){
+    return Tasks.find({
+      // Note: Only publish tasks that are public or belong to the current user
+      $or: [
+        { private : {$ne: true}},
+        {owner: this.userId}
+      ]
+    });
+  });
+
   Meteor.startup(function () {
     // code to run on server at startup
   });
